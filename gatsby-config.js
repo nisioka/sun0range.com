a47786fe04f5b3e7584494946aaf8e024575a586
nisioka <16322296+nisioka@users.noreply.github.com>
2024-05-11 08:46:33 +0900

    favicon変更。

--- gatsby-config.js
+++ gatsby-config.js
@@ -109,15 +109,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `分かりやすい技術ブログ`,
+        short_name: `分か技ブログ`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/fabicon.webp`, // This path is relative to the root of the site.
       },
     },
     {
